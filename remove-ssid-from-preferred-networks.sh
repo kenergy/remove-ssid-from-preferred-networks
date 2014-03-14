4dee8b9ddfba4eb46bce602755c02ab05c5a0997
Jacob Salmela <jakesalmela@users.noreply.github.com>
2014-03-14 09:49:37 -0500

    Create remove-ssid-from-preferred-networks.sh

---
+++ remove-ssid-from-preferred-networks.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+#----------AUTHOR------------
+	# Jacob Salmela
+	# 26 November 2012
+
+#----------RESOURCES---------
+	# Removes a SSID from the list of preferred networks
+
+#---------DESCRIPTION--------
+	# https://jamfnation.jamfsoftware.com/discussion.html?id=5327
+
+#----------VARIABLES---------	
+	# Enter desired and undesired SSIDs here
+	undesiredNetwork="Guest-Hopkins"
+	desiredNetwork="Desired-SSID"
+
+	# Determine if the interface is called Wi-Fi or Airport
+	wifiOrAirport=$(networksetup -listallnetworkservices | grep -Ei '(Wi-Fi|AirPort)')
+
+	# Determine what the device interface number is
+	wirelessDevice=$(networksetup -listallhardwareports | awk "/$wifiOrAirport/,/Device/" | awk 'NR==2' | cut -d " " -f 2)
+
+#----------FUNCTIONS---------
+#################################
+function removeUndesiredNetwork() 
+	{
+	case $(networksetup -listpreferredwirelessnetworks $wirelessDevice | awk '/'"$undesiredNetwork"'/ {print $1}') in
+		"$undesiredNetwork") networksetup -removepreferredwirelessnetwork "$wirelessDevice" "$undesiredNetwork";
+							return 0;;
+		*) echo "$undesiredNetwork is NOT in the preferred list.";
+							return 1;;
+	esac
+	}
+
+#----------------------------
+#-----------SCRIPT-----------
+#----------------------------
+removeUndesiredNetwork
